24619d0de964edb6e263ab1a7ceb6460486c443f
Mohan T <mohans.tamada@gmail.com>
2026-03-17 22:36:01 -0500

    updated assignment 7

--- assignments/assignment7-stringOps.ts
+++ assignments/assignment7-stringOps.ts
@@ -1,29 +1,28 @@
 let mystring:string = "Java programming is fun and challenging";
 let words : string[] = mystring.split(" ");
 //1. Count the total number of words in the sentence.
-console.log(`Total number of the words in the sentence ${mystring} is ${words.length}`);
+console.log(`7_1.Total number of the words in the sentence "${mystring}" is ${words.length}`);
 
 //2. Print the sentence words in reverse order.
 let reverseMyString: string[] = [];
 
 let index:number = 0
+let reversewords:string = ""
 for (let i:number=words.length-1;i>=0;i--){
-    console.log(words[i])
     for (let j:number=index;j<=words.length-1;j++){
         reverseMyString[index] = words[i];
-        console.log(reverseMyString[j])
+        reversewords+=reverseMyString[index]+" ";
         index+=1;
         break;
     }
 }
-console.log(`Print the sentence words of ${mystring} in reverse order${reverseMyString}`);
+console.log(`7_2. Print the sentence words of "${mystring}" in reverse order "${reversewords}"`);
 
 //Convert the first character of each word to uppercase and print original sentence
+let newstring:string = "";
 for (let i:number=0;i<words.length;i++){
-    let firstChar:string = words[i].charAt(0);
-    console.log(firstChar)
-    console.log(words[i])
+    let firstChar:string = words[i].charAt(0);    
     words[i] = words[i].replace(firstChar,firstChar.toUpperCase());
-    console.log(words[i])
+    newstring+=words[i]+" ";
 }
-console.log(words)
+console.log(`7_3. The new string with first character of each word convered to uppercase : "${newstring}"`);
